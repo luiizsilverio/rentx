a39b544cc56d88265c3c149c4a2970e39f1189df
luiizsilverio <luiiz.silverio@gmail.com>
2021-07-22 16:13:25 -0300

    Feat: Acessories

--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native'
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -19,3 +20,69 @@ export const Header = styled.View`
 export const SliderContainer = styled.View`
   margin-top: ${getStatusBarHeight() + 32}px;
 `
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: { 
+    padding: 24,
+    alignItems: 'center'
+  },
+  showsVerticalScrollIndicator: false
+})``;
+
+export const Details = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 38px;
+`;
+
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${props => props.theme.fonts.secondary_500};
+  color: ${props => props.theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+  text-transform: uppercase;
+`;
+
+export const Name = styled.Text`
+  font-family: ${props => props.theme.fonts.secondary_500};
+  color: ${props => props.theme.colors.title};
+  font-size: ${RFValue(25)}px;
+  text-transform: uppercase;
+`;
+
+export const Period = styled.Text`
+  font-family: ${props => props.theme.fonts.secondary_500};
+  color: ${props => props.theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+  text-transform: uppercase;
+  `;
+
+export const Price = styled.Text`
+  font-family: ${props => props.theme.fonts.secondary_500};
+  color: ${props => props.theme.colors.main};
+  font-size: ${RFValue(25)}px;
+  text-transform: uppercase;
+`;
+
+export const Rent = styled.View``;
+
+export const About = styled.Text`
+  font-family: ${props => props.theme.fonts.primary_400};
+  color: ${props => props.theme.colors.text};
+  font-size: ${RFValue(15)}px;
+  text-align: justify;
+  margin-top: 23px;
+  line-height: ${RFValue(25)}px;
+`;
+
+export const Acessories = styled.View`
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+`;
